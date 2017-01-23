ecc39e38d0ecc5fb51a042d376b217f39036b17f
Kennedy Opoku Asare <opokuasarekennedy@gmail.com>
2017-01-23 18:52:44 +0200

    Finalise Code

--- app/src/main/java/fi/oulu/mobisocial/share/MainActivity.java
+++ app/src/main/java/fi/oulu/mobisocial/share/MainActivity.java
@@ -1,47 +1,51 @@
 package fi.oulu.mobisocial.share;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.support.design.widget.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button aboutBtn;
+    EditText textView;
+    FloatingActionButton share_floatButn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.about_btn).setOnClickListener(new EventHandlers());
-        findViewById(R.id.share_floatButn).setOnClickListener(new EventHandlers());
+        aboutBtn = (Button) findViewById(R.id.about_btn);
+        textView = (EditText) findViewById(R.id.editText);
+        share_floatButn = (FloatingActionButton) findViewById(R.id.share_floatButn);
 
-    }
 
-    private class EventHandlers implements View.OnClickListener {
+        aboutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent about = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(about);
 
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.about_btn:
-                    Intent about = new Intent(MainActivity.this, AboutActivity.class);
-                    startActivity(about);
-                    Log.d("Asare", "button Click");
-                    break;
-                case R.id.share_floatButn:
-                    Intent shareIntent = new Intent();
-                    shareIntent.setAction(Intent.ACTION_SEND);
-                    shareIntent.putExtra(Intent.EXTRA_TEXT, ((TextView) findViewById(R.id.editText)).getText().toString());
-                    shareIntent.setType("text/plain");
-                    startActivity(shareIntent);
-                    Log.d("Asare", "Float button  Click");
-                    break;
             }
+        });
 
-        }
+        share_floatButn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareIntent = new Intent();
+                shareIntent.setAction(Intent.ACTION_SEND);
+                shareIntent.putExtra(Intent.EXTRA_TEXT, textView.getText().toString());
+                shareIntent.setType("text/plain");
+                Intent.createChooser(shareIntent, "Share Via");
+                startActivity(shareIntent);
+                Log.d("Asare", "Float button  Click");
+            }
+        });
+
     }
 }
